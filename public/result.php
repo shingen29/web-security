91488216058efb596f0cf15e6947795d49623432
Makoto Otsuka <mametra2580@gmail.com>
2020-12-24 12:54:29 +0900

    [add]XSS対策

---
+++ public/result.php
@@ -0,0 +1,7 @@
+<html>
+<head>
+</head>
+<body>
+<h2><?php echo htmlspecialchars($_GET['keyword']); ?>の検索結果</h2>
+</body>
+</html>
---
+++ public/search.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+</head>
+<body>
+<form action="result.php">
+    <input type="text" name="keyword">
+    <button type="submit">検索</button>
+</form>
+</body>
+</html>
